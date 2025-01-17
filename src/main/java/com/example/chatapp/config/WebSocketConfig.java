3744b1a1f15dda18d1706c360a99ffd856d72f7d
“kupid” <shiftybroccoli74@gmail.com>
2025-01-17 19:27:24 -0300

    feat(websocket): configure WebSocket with STOMP and message broker

--- src/main/java/com/example/chatapp/config/WebSocketConfig.java
+++ src/main/java/com/example/chatapp/config/WebSocketConfig.java
@@ -1,5 +1,30 @@
 package com.example.chatapp.config;
 
-public class WebSocketConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer{
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        //definimos un "chatroom" donde todos vemos el msj que llegue a /topic
+        registry.enableSimpleBroker("/topic");
+        //todos los msj que lleguen con el prefijo /app los procese
+        registry.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        //definimos el endpoint del chat
+        registry.addEndpoint("/chat")
+                .setAllowedOrigins("/http://localhost:8080")
+                .withSockJS();
+    }
+
+    
 }
